7a6959935dffe77120ad3f9974e23c4903da0725
Miguel Areias <miguel-areias@dcc.fc.up.pt>
2018-08-09 16:43:27 +0100

    hash -> check different positions

---
+++ java/HashLinear.java
@@ -0,0 +1,49 @@
+
+// All buckets from H_i refer to the same H_i+1 
+public class HashLinear {
+    private static int size;  // size of each hash level
+    private static int depth; // number of hash levels 
+    private static Object[] root;
+    private static int second_touch;
+    
+	HashLinear(int size, int depth, int second_touch) {
+	root = new Object[size];
+	if (second_touch > size - 1)
+	    this.second_touch = size -1;
+	else
+	    this.second_touch = second_touch;
+      
+	Object[] curr_hash = root;
+		
+	for (int d = 1; d < depth; d++) {
+	    Object[] next_hash = new Object[size];
+	    for (int s = 0; s < size - 1; s++)
+		curr_hash[s] = next_hash;
+	    curr_hash = next_hash;   
+	}
+	// last hash entries remain with 'null' values 
+    }
+
+    void check_entry_one_touch(int i) {
+	Object[] curr_hash = root;	
+	for (int d = 0; d < depth; d++) {
+	    Object h = curr_hash[i];
+	    curr_hash = (Object []) h;
+	}
+	curr_hash[0] = (Object) 10;
+    }
+
+
+    void check_entry_two_touch(int i, int second_touch) {
+	Object[] curr_hash = root;	
+	for (int d = 0; d < depth; d++) {
+	    // first touch
+	    Object h = curr_hash[i];
+	    h = curr_hash[i];
+	    h = curr_hash[second_touch];
+	    curr_hash = (Object []) h;
+	}
+    }
+
+    
+}
---
+++ java/TestSearchHashLinear.java
@@ -0,0 +1,84 @@
+
+
+public class TestSearchHashLinear {
+    // configurator
+    private static int size = 64;  // size of each hash level
+    private static int depth = 1000000; // number of hash levels
+    private static int threads = 0; // 0 - main thread only / n - n threads
+    private static int entry_i = 0;
+    private static int runs = 1;
+    private static int second_touch = 48; 
+    
+    public static void main(final String[] args) {
+	System.out.println("time (setup)");
+	HashLinear hash = new HashLinear(size, depth, second_touch); 
+	if (threads == 0)
+	    //	    runSingleThreadOneTouch(hash);
+	runSingleThreadTwoTouch(hash);	
+	//	else
+	//    runMultiThread(hash);
+    }
+
+    private static void runSingleThreadOneTouch(HashLinear hash) {
+	System.out.println("time (run)");
+	long start =  System.nanoTime();
+	hash.check_entry_one_touch(entry_i);
+	long end = System.nanoTime();
+	long time = (end - start) / 1000000L;
+	long avg_first_time = time;
+	long avg_second_time = 0;
+	for (int r = 1; r < runs; r++) {
+	    start =  System.nanoTime();
+	    hash.check_entry_one_touch(entry_i);
+	    end = System.nanoTime();
+	    time = (end - start) / 1000000L;
+	    avg_first_time += time;
+	    avg_second_time += time;
+	}
+
+	System.out.println("Main - First Run Time = " + time + " ms");
+
+	System.out.println("Main - Avg with first Run Time = " 
+			 +  avg_first_time /runs + " ms");
+
+	if (runs > 1)
+	    System.out.println("Main - Avg without first Run Time = "
+			       + avg_second_time / (runs - 1) + " ms");
+	else
+	    System.out.println("Main - Avg without first Run Time = "
+			       + avg_second_time / runs + " ms");
+    }
+
+    private static void runSingleThreadTwoTouch(HashLinear hash) {
+	System.out.println("time (run)");
+	long start =  System.nanoTime();
+	hash.check_entry_two_touch(entry_i, second_touch);
+	long end = System.nanoTime();
+	long time = (end - start) / 1000000L;
+	long avg_first_time = time;
+	long avg_second_time = 0;
+	for (int r = 1; r < runs; r++) {
+	    start =  System.nanoTime();
+	    hash.check_entry_two_touch(entry_i, second_touch);
+	    end = System.nanoTime();
+	    time = (end - start) / 1000000L;
+	    avg_first_time += time;
+	    avg_second_time += time;
+	}
+
+	System.out.println("Main - First Run Time = " + time + " ms");
+
+	if (runs > 1)
+	    System.out.println("Main - Avg without first Run Time = "
+			       + avg_second_time / (runs - 1) + " ms");
+	else
+	    System.out.println("Main - Avg without first Run Time = "
+			       + avg_second_time / runs + " ms");
+
+
+    }
+
+
+
+    
+}
